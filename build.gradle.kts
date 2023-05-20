749e74746a73f749cb4cc5f00b9fb26f5d1762fa
takusan23 <takumu1200@gmail.com>
2023-05-21 00:12:12 +0900

    beta である必要がないので修正

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application").version("8.1.0-beta02").apply(false)
-    id("com.android.library").version("8.1.0-beta02").apply(false)
+    id("com.android.application").version("8.0.1").apply(false)
+    id("com.android.library").version("8.0.1").apply(false)
     id("org.jetbrains.kotlin.android").version("1.8.21").apply(false)
     id("org.jetbrains.kotlin.jvm").version("1.8.21").apply(false)
 }
@@ -13,6 +13,7 @@ tasks.register("clean") {
 }
 
 // TODO 何故か必要。Android Gradle Plugin の更新で要らなくなるはず
+// https://issuetracker.google.com/issues/278118298
 subprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions {
