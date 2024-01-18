61b4cb2d4348abf3faa3cf0cb6f52a49780f6331
takusan23 <takumu1200@gmail.com>
2024-01-19 00:01:07 +0900

    enableEdgeToEdge に置き換え

--- app/src/main/java/io/github/takusan23/zeromirror/MainActivity.kt
+++ app/src/main/java/io/github/takusan23/zeromirror/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.takusan23.zeromirror
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.github.takusan23.zeromirror.ui.screen.MainScreen
 
@@ -11,6 +12,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         // スプラッシュスクリーンを出す
         installSplashScreen()
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         setContent { MainScreen() }
